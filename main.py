0cf00c05e565c055250f9eda157d37f7caf10f4d
martomarzo <martinemarzorati@gmail.com>
2024-09-09 10:40:46 +0200

    added player render

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pygame
 from constants import * 
+from player import *
 
 
 
@@ -8,19 +9,28 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    clock= pygame.time.Clock()
+    dt = 0
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        pygame.Surface.fill(screen,color = (0,0,0))
+        screen.fill(color = (0,0,0))
+        player.draw(screen)        
+        dt=clock.tick_busy_loop(60)
         pygame.display.flip()
-    
-    
+        clock.tick(60)            
+        
+        
+        
     
     
 if __name__ == "__main__":
